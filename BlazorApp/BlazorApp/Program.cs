74b7eb7e31f7639610c02c481a5e3a26995135e1
Uta Sugane <sugane.ns10@gmail.com>
2023-10-01 09:49:14 +0900

    first commit

---
+++ BlazorApp/BlazorApp/Program.cs
@@ -0,0 +1,39 @@
+using BlazorApp.Data;
+using GrpcService;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// 追記する
+builder.Services.AddGrpcClient<Greeter.GreeterClient>(o =>
+{
+    // バックエンドのlaunchSettings.jsonのhttpsのapplicationUrlに記載されていたUrlを書く
+    o.Address = new Uri("https://localhost:7280");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
